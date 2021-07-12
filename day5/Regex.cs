691a64bea851b48b01a10b46546a682dfc881a9e
vijayjayakumar02 <vijayjayakumar0206@gmail.com>
2021-07-12 10:19:46 +0530

    commits

---
+++ day5/Regex.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Text.RegularExpressions;
+
+namespace day5
+{
+    class regex
+    {
+        public void reg_exp()
+        {
+            string pass = Console.ReadLine();
+            Console.WriteLine(CheckPassword(pass));
+        }
+        public bool CheckPassword(string password)
+        {
+            //matching the pattern
+            string MatchEmailPattern = "^[a-zA-Z0-9]+$";
+
+            if (password != null) 
+                return Regex.IsMatch(password, MatchEmailPattern);
+            else return false;
+
+
+        }
+    }
+}
