af9018532eae7cac8251ac510c4d2be1f35f000e
vijayjayakumar02 <vijayjayakumar0206@gmail.com>
2021-07-17 18:28:16 +0530

    commits

---
+++ Demo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[5];
+            for (int i = 0; i < 5; i++)
+            {
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            foreach (int element in arr)
+            {
+                Console.Write(element + " ");
+            }
+        }
+    }
+}
