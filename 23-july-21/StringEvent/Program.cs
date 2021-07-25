c9cd4f3022295592ce95bb200d515278a4ca6745
vijayjayakumar02 <vijayjayakumar0206@gmail.com>
2021-07-25 21:50:58 +0530

    commits

---
+++ 23-july-21/StringEvent/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StringEvent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
