3d5af67ace20bd0b912d46c80d001d8f49de3439
vijayjayakumar02 <vijayjayakumar0206@gmail.com>
2021-07-25 14:38:35 +0530

    commits

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace Demo
 {
@@ -6,7 +7,30 @@ namespace Demo
     {
         static void Main(string[] args)
         {
-            
+            Method1();
+            Method2();
+            Console.ReadKey();
+
+        }
+        public static async Task Method1()
+        {
+            await Task.Run(() =>
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    System.Console.WriteLine("method 1");
+                    Task.Delay(100).Wait();
+                }
+            });
+        }
+        public static void Method2()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine("method 2");
+                Task.Delay(100).Wait();
+            }
+
         }
     }
 }
